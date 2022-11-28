af650278dcd939be689af8c21b8859f992adbe1f
cxl229 <cxl229@nyu.edu>
2022-11-28 17:23:19 -0500

    added art for gacha machine, opponents, and cats!

--- src/catProfiles.mjs
+++ src/catProfiles.mjs
@@ -3,23 +3,23 @@ import {FighterProfile, commonPercentage, uncommonPercentage, rarePercentage, le
 
 export const cats = [
     // common cats
-    new FighterProfile("Ginger", "Fierce orange warrior", "cat", "/img/test.png", 50, 5, 0.03, "common", commonPercentage),
-    new FighterProfile("Coffee", "Caffeine-fueled fighter", "cat", "/img/test.png", 50, 5, 0.03, "common", commonPercentage),
-    new FighterProfile("Chi", "Sweet homebody", "cat", "/img/test.png", 50, 5, 0.03, "common", commonPercentage),
-    new FighterProfile("Tux", "Fanciest cat on the block", "cat", "/img/test.png", 40, 6, 0.03, "common", commonPercentage),
-    new FighterProfile("Midnight", "Stealthy spy", "cat", "/img/test.png", 40, 6, 0.04, "common", commonPercentage),
-    new FighterProfile("Shadow", "Mysterious wanderer", "cat", "/img/test.png", 60, 4, 0.04, "common", commonPercentage),
-    new FighterProfile("Mittens", "Sanguine sleepyhead", "cat", "/img/test.png", 60, 4, 0.04, "common", commonPercentage),
+    new FighterProfile("Ginger", "Fierce orange warrior", "cat", "/img/cats/ginger.png", 50, 5, 0.03, "common", commonPercentage),
+    new FighterProfile("Coffee", "Caffeine-fueled fighter", "cat", "/img/cats/coffee.png", 50, 5, 0.03, "common", commonPercentage),
+    new FighterProfile("Chi", "Sweet homebody", "cat", "/img/cats/chi.png", 50, 5, 0.03, "common", commonPercentage),
+    new FighterProfile("Tux", "Fanciest cat on the block", "cat", "/img/cats/tux.png", 40, 6, 0.03, "common", commonPercentage),
+    new FighterProfile("Midnight", "Stealthy spy", "cat", "/img/cats/midnight.png", 40, 6, 0.04, "common", commonPercentage),
+    new FighterProfile("Shadow", "Mysterious wanderer", "cat", "/img/cats/shadow.png", 60, 4, 0.04, "common", commonPercentage),
+    new FighterProfile("Mittens", "Sanguine sleepyhead", "cat", "/img/cats/mittens.png", 60, 4, 0.04, "common", commonPercentage),
     // uncommon cats
-    new FighterProfile("Potato chip", "Snack-loving siamese", "cat", "/img/test.png", 60, 7, 0.05, "uncommon", uncommonPercentage),
-    new FighterProfile("Princess Snowball", "Do not disrespect the princess!", "cat", "/img/test.png", 60, 7, 0.05, "uncommon", uncommonPercentage),
-    new FighterProfile("Panda", "Cute but deadly", "cat", "/img/test.png", 50, 8, 0.06, "uncommon", uncommonPercentage),
-    new FighterProfile("Fluffy", "The fluffiest feline", "cat", "/img/test.png", 70, 6, 0.06, "uncommon", uncommonPercentage),
-    new FighterProfile("Shelly", "Sophisticated sweetie", "cat", "/img/test.png", 70, 6, 0.06, "uncommon", uncommonPercentage),
+    new FighterProfile("Potato chip", "Snack-loving siamese", "cat", "/img/cats/potatochip.png", 60, 7, 0.05, "uncommon", uncommonPercentage),
+    new FighterProfile("Princess Snowball", "Do not disrespect the princess!", "cat", "/img/cats/princesssnowball.png", 60, 7, 0.05, "uncommon", uncommonPercentage),
+    new FighterProfile("Panda", "Cute but deadly", "cat", "/img/cats/panda.png", 50, 8, 0.06, "uncommon", uncommonPercentage),
+    new FighterProfile("Fluffy", "The fluffiest feline", "cat", "/img/cats/fluffy.png", 70, 6, 0.06, "uncommon", uncommonPercentage),
+    new FighterProfile("Shelly", "Sophisticated sweetie", "cat", "/img/cats/shelly.png", 70, 6, 0.06, "uncommon", uncommonPercentage),
     // rare cats
-    new FighterProfile("Mr. Big Paws", "Humongous harbinger", "cat", "/img/test.png", 80, 8, 0.07, "rare", rarePercentage),
-    new FighterProfile("Beanie", "Beguiling beauty", "cat", "/img/test.png", 80, 8, 0.08, "rare", rarePercentage),
+    new FighterProfile("Mr. Big Paws", "Humongous harbinger", "cat", "/img/cats/mrbigpaws.png", 80, 8, 0.07, "rare", rarePercentage),
+    new FighterProfile("Beanie", "Beguiling beauty", "cat", "/img/cats/beanie.png", 80, 8, 0.08, "rare", rarePercentage),
     // legendary cats
-    new FighterProfile("Tony", "Might just be a tiger", "cat", "/img/test.png", 80, 11, 0.1, "legendary", legendaryPercentage),
-    new FighterProfile("Prisma", "Elusive elegance", "cat", "/img/test.png", 100, 10, 0.15, "legendary", legendaryPercentage),
+    new FighterProfile("Tony", "Might just be a tiger", "cat", "/img/cats/tony.png", 80, 11, 0.1, "legendary", legendaryPercentage),
+    new FighterProfile("Prisma", "Elusive elegance", "cat", "/img/cats/prisma.png", 100, 10, 0.15, "legendary", legendaryPercentage),
 ];
--- src/opponentProfiles.mjs
+++ src/opponentProfiles.mjs
@@ -7,25 +7,25 @@ const chance = new Chance();
 
 const opponents = [
     // common opponents
-    new FighterProfile("Cheesy", "Little guy who loves cheese", "opponent", "/img/test.png", 10, 4, 0, "common", commonPercentage),
-    new FighterProfile("Toast", "Here to steal your cat treats", "opponent", "/img/test.png", 10, 4, 0, "common", commonPercentage),
-    new FighterProfile("#10347", "Escaped from the lab", "opponent", "/img/test.png", 10, 4, 0, "common", commonPercentage),
-    new FighterProfile("Elvis", "Sardonic singer", "opponent", "/img/test.png", 15, 5, 0.01, "common", commonPercentage),
-    new FighterProfile("Pete", "Sailed the seven seas", "opponent", "/img/test.png", 15, 5, 0.01, "common", commonPercentage),
-    new FighterProfile("Muddy", "The horrible hopper", "opponent", "/img/test.png", 15, 5, 0.02, "common", commonPercentage),
-    new FighterProfile("Coco", "Cat-hating chihuahua", "opponent", "/img/test.png", 20, 5, 0.02, "common", commonPercentage),
+    new FighterProfile("Cheesy", "Little guy who loves cheese", "opponent", "/img/opponents/cheesy.png", 10, 4, 0, "common", commonPercentage),
+    new FighterProfile("Toast", "Here to steal your cat treats", "opponent", "/img/opponents/toast.png", 10, 4, 0, "common", commonPercentage),
+    new FighterProfile("#10347", "Escaped from the lab", "opponent", "/img/opponents/10347.png", 10, 4, 0, "common", commonPercentage),
+    new FighterProfile("Elvis", "Sardonic singer", "opponent", "/img/opponents/elvis.png", 15, 5, 0.01, "common", commonPercentage),
+    new FighterProfile("Pete", "Sailed the seven seas", "opponent", "/img/opponents/pete.png", 15, 5, 0.01, "common", commonPercentage),
+    new FighterProfile("Muddy", "The horrible hopper", "opponent", "/img/opponents/muddy.png", 15, 5, 0.02, "common", commonPercentage),
+    new FighterProfile("Coco", "Cat-hating chihuahua", "opponent", "/img/opponents/coco.png", 20, 5, 0.02, "common", commonPercentage),
     // uncommon opponents
-    new FighterProfile("Merlin", "Wise warrior", "opponent", "/img/test.png", 20, 7, 0.03, "uncommon", uncommonPercentage),
-    new FighterProfile("New York Nick", "He's walkin' here!", "opponent", "/img/test.png", 20, 7, 0.03, "uncommon", uncommonPercentage),
-    new FighterProfile("Genji", "Merciless magpie", "opponent", "/img/test.png", 20, 8, 0.04, "uncommon", uncommonPercentage),
-    new FighterProfile("Boris", "Bloodthirsty battler", "opponent", "/img/test.png", 25, 8, 0.04, "uncommon", uncommonPercentage),
-    new FighterProfile("Big Mac", "Ruthless raccoon", "opponent", "/img/test.png", 25, 8, 0.04, "uncommon", uncommonPercentage),
+    new FighterProfile("Merlin", "Wise warrior", "opponent", "/img/opponents/merlin.png", 20, 7, 0.03, "uncommon", uncommonPercentage),
+    new FighterProfile("New York Nick", "He's walkin' here!", "opponent", "/img/opponents/newyorknick.png", 20, 7, 0.03, "uncommon", uncommonPercentage),
+    new FighterProfile("Genji", "Merciless magpie", "opponent", "/img/opponents/genji.png", 20, 8, 0.04, "uncommon", uncommonPercentage),
+    new FighterProfile("Boris", "Bloodthirsty battler", "opponent", "/img/opponents/boris.png", 25, 8, 0.04, "uncommon", uncommonPercentage),
+    new FighterProfile("Big Mac", "Ruthless raccoon", "opponent", "/img/opponents/bigmac.png", 25, 8, 0.04, "uncommon", uncommonPercentage),
     // rare opponents
-    new FighterProfile("Persimmon", "Battle bear", "opponent", "/img/test.png", 30, 10, 0.05, "rare", rarePercentage),
-    new FighterProfile("Kit", "Sneaky fox", "opponent", "/img/test.png", 50, 9, 0.06, "rare", rarePercentage),
+    new FighterProfile("Persimmon", "Battle bear", "opponent", "/img/opponents/persimmon.png", 30, 10, 0.05, "rare", rarePercentage),
+    new FighterProfile("Kit", "Sneaky fox", "opponent", "/img/opponents/kit.png", 50, 9, 0.06, "rare", rarePercentage),
     // legendary opponents
-    new FighterProfile("Draco", "Thieving jewel hoarder", "opponent", "/img/test.png", 70, 15, 0.08, "legendary", legendaryPercentage),
-    new FighterProfile("Void", "???", "opponent", "/img/test.png", 70, 20, 0.11, "legendary", legendaryPercentage),
+    new FighterProfile("Draco", "Thieving jewel hoarder", "opponent", "/img/opponents/draco.png", 70, 15, 0.08, "legendary", legendaryPercentage),
+    new FighterProfile("Void", "???", "opponent", "/img/opponents/void.png", 70, 20, 0.11, "legendary", legendaryPercentage),
 ];
 
 // Get an opponent profile based on its name
